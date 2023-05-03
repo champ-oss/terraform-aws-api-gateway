62500089f07902e3fdb91f48ab6b00b887300a6c
Justin Reeves <reeves122@gmail.com>
2023-05-03 15:31:23 -0500

    add tf outputs

--- examples/shared_modules/lb_ecs/outputs.tf
+++ examples/shared_modules/lb_ecs/outputs.tf
@@ -1,19 +1,24 @@
 output "lb_public_listener_arn" {
-  value = module.core.lb_public_listener_arn
+  description = "core module public lb listener arn"
+  value       = module.core.lb_public_listener_arn
 }
 
 output "lb_private_listener_arn" {
-  value = module.core.lb_private_listener_arn
+  description = "core module private lb listener arn"
+  value       = module.core.lb_private_listener_arn
 }
 
 output "ecs_app_security_group" {
-  value = module.core.ecs_app_security_group
+  description = "core module security group for ecs"
+  value       = module.core.ecs_app_security_group
 }
 
 output "private_subnet_ids" {
-  value = data.aws_subnets.private.ids
+  description = "list of private vpc subnets"
+  value       = data.aws_subnets.private.ids
 }
 
 output "dns_name" {
-  value = "${local.hostname}.${data.aws_route53_zone.this.name}"
+  description = "DNS name used for ecs application"
+  value       = "${local.hostname}.${data.aws_route53_zone.this.name}"
 }

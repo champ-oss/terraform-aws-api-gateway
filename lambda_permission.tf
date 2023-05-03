e3fa05798978d27e662b962b3a40a4fa6eda289f
Justin Reeves <reeves122@gmail.com>
2023-05-03 12:06:16 -0500

    fix count on aws_apigatewayv2_vpc_link and aws_lambda_permission

--- lambda_permission.tf
+++ lambda_permission.tf
@@ -1,4 +1,5 @@
-resource "aws_lambda_permission" "apigw" {
+resource "aws_lambda_permission" "this" {
+  count         = var.enable_lambda_integration ? 1 : 0
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_invoke_arn
   principal     = "apigateway.amazonaws.com"

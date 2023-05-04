db24c044cfa673d3ab544361cd822cf927dfeb5b
Justin Reeves <reeves122@gmail.com>
2023-05-04 11:43:16 -0500

    add lambda with whitelisting example

---
+++ examples/lambda_with_whitelisting/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "aws_route53_zone" "this" {
+  name = "oss.champtest.net."
+}
+
+# Deploy a Lambda for API Gateway integration
+module "lambda" {
+  source = "../shared_modules/python_lambda"
+}
+
+resource "random_string" "this" {
+  length  = 5
+  special = false
+  upper   = false
+  lower   = true
+  numeric = true
+}
+
+locals {
+  git      = "terraform-aws-api-gateway"
+  hostname = "terraform-aws-api-gateway-${random_string.this.result}"
+}
+
+module "this" {
+  source                    = "../../"
+  git                       = local.git
+  domain_name               = "${local.hostname}.${data.aws_route53_zone.this.name}"
+  zone_id                   = data.aws_route53_zone.this.zone_id
+  enable_create_certificate = true
+  enable_lambda_integration = true
+  lambda_invoke_arn         = module.lambda.arn
+  enable_api_gateway_v1     = true
+}

b6314c331a11f5148349406f665c6dee84f21d43
Justin Reeves <reeves122@gmail.com>
2023-05-05 11:32:51 -0500

    use name_prefix for policies

--- iam.tf
+++ iam.tf
@@ -1,15 +1,15 @@
 resource "aws_iam_role" "this" {
   count              = var.enable_api_gateway_v1 ? 1 : 0
-  name               = "${var.git}-apigateway-${random_string.this.result}"
+  name_prefix        = "${var.git}-apigateway-${random_string.this.result}-"
   assume_role_policy = data.aws_iam_policy_document.assume_role[0].json
   tags               = merge(local.tags, var.tags)
 }
 
 resource "aws_iam_role_policy" "this" {
-  count  = var.enable_api_gateway_v1 ? 1 : 0
-  name   = "${var.git}-apigateway-${random_string.this.result}"
-  role   = aws_iam_role.this[0].id
-  policy = data.aws_iam_policy_document.cloudwatch[0].json
+  count       = var.enable_api_gateway_v1 ? 1 : 0
+  name_prefix = "${var.git}-apigateway-${random_string.this.result}-"
+  role        = aws_iam_role.this[0].id
+  policy      = data.aws_iam_policy_document.cloudwatch[0].json
 }
 
 data "aws_iam_policy_document" "assume_role" {

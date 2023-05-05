3fd17736b945a9d8a39133eddacfa893fc5dc748
Justin Reeves <reeves122@gmail.com>
2023-05-05 11:41:12 -0500

    use substring for iam names

--- iam.tf
+++ iam.tf
@@ -1,13 +1,13 @@
 resource "aws_iam_role" "this" {
   count              = var.enable_api_gateway_v1 ? 1 : 0
-  name_prefix        = "${var.git}-apigateway-${random_string.this.result}-"
+  name_prefix        = substr("${var.git}-apigateway-${random_string.this.result}-", 0, 36)
   assume_role_policy = data.aws_iam_policy_document.assume_role[0].json
   tags               = merge(local.tags, var.tags)
 }
 
 resource "aws_iam_role_policy" "this" {
   count       = var.enable_api_gateway_v1 ? 1 : 0
-  name_prefix = "${var.git}-apigateway-${random_string.this.result}-"
+  name_prefix = substr("${var.git}-apigateway-${random_string.this.result}-", 0, 36)
   role        = aws_iam_role.this[0].id
   policy      = data.aws_iam_policy_document.cloudwatch[0].json
 }

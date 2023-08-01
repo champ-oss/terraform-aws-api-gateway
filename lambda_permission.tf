9228edf00ca6de89cfcc80c9ea9e804a83da5ce1
Justin Reeves <reeves122@gmail.com>
2023-08-01 15:16:47 -0500

    remove aws_lambda_permission

--- lambda_permission.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_lambda_permission" "api_gateway_v1" {
-  count         = var.enable_lambda_integration ? 1 : 0
-  action        = "lambda:InvokeFunction"
-  function_name = var.lambda_arn
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${data.aws_region.this.name}:${data.aws_caller_identity.this.account_id}:${aws_api_gateway_rest_api.this.id}/*/*/"
-}

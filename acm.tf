f6c55097e73bd4d492391ea4abc65431a0f52204
Justin Reeves <reeves122@gmail.com>
2023-08-02 09:30:02 -0500

    update terraform-aws-acm

--- acm.tf
+++ acm.tf
@@ -1,11 +1,10 @@
 module "acm" {
   count             = var.enable_create_certificate ? 1 : 0
-  source            = "github.com/champ-oss/terraform-aws-acm.git?ref=v1.0.111-28fcc7c"
+  source            = "github.com/champ-oss/terraform-aws-acm.git?ref=use-validation-arn"
   git               = var.git
   domain_name       = var.domain_name
   create_wildcard   = false
   zone_id           = var.zone_id
   enable_validation = true
-  time_sleep        = 60
   tags              = merge(local.tags, var.tags)
 }

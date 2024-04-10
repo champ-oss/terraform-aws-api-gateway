47eb93640b5ee4e0a7c4f70757721351870bc135
Justin Reeves <reeves122@gmail.com>
2024-04-10 09:32:40 -0500

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -51,7 +51,7 @@ data "archive_file" "this" {
 }
 
 module "lambda1" {
-  source                         = "github.com/champ-oss/terraform-aws-lambda.git?ref=main"
+  source                         = "github.com/champ-oss/terraform-aws-lambda.git"
   git                            = "terraform-aws-api-gateway"
   name                           = "lambda1"
   filename                       = data.archive_file.this.output_path
@@ -71,7 +71,7 @@ module "lambda1" {
 }
 
 module "lambda2" {
-  source                         = "github.com/champ-oss/terraform-aws-lambda.git?ref=main"
+  source                         = "github.com/champ-oss/terraform-aws-lambda.git"
   git                            = "terraform-aws-api-gateway"
   name                           = "lambda2"
   filename                       = data.archive_file.this.output_path
